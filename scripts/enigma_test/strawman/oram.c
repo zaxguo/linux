118e116ec3ed7f5cf269dd0fe7fe1c9d91284e05
Liwei Guo <zaxguo@me.com>
2021-03-27 17:35:42 -0400

    udpate oram strawman

--- scripts/enigma_test/strawman/oram.c
+++ scripts/enigma_test/strawman/oram.c
@@ -4,18 +4,161 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include "oram.h"
+#include <math.h>
+#include <string.h>
+
+
+/* global var */
+int *pos_map;
+int **path_map;
+struct stash *stash;
+struct oram_tree_param *param;
+struct bucket **buckets;
+
+/* a flat array representation of bucket tree on disk
+ * node #:		0   1		2		3		....
+ * file off:	0   4*512	2*4*512 3*4*512	...
+ * tree:
+ *				0
+ *			  /   \
+ *		     1     2
+ *		    / \   / \
+ *		   3   4  5  6
+ *
+ * */
+static loff_t get_bucket_file_off(int bucket_no) {
+	return (bucket_no * (BUCKET_SIZE * BLOCK_SIZE));
+}
+
+static int get_leaf_offset() {
+	return param->leaf_offset;
+}
+
+static int get_random_leaf(struct oram_tree_param *param) {
+	return ((rand() % param->blocks) + param->leaf_offset);
+}
+
+static void print_path(int *path, int tree_height) {
+	for (int i = 0; i < tree_height; i++) {
+		printf("[%d] ", path[i]);
+	}
+	printf("\n");
+	return;
+}
+
+static void dump_bucket(int bid) {
+	struct bucket *bkt = buckets[bid];
+	printf("[%d:%p] contains: ", bid, bkt);
+	for (int i = 0; i < BUCKET_SIZE; i++) {
+		printf("%d ", bkt->b_list[i]);
+	}
+	printf("\n");
+}
+
+static void insert_path(int *path, int idx) {
+	int *tmp = malloc(sizeof(int) * (param->height + 1));
+	memcpy(tmp, path, sizeof(int) * (param->height + 1));
+	path_map[idx] = tmp;
+	/* debugging -- print height + 1 nodes as path */
+	printf("inserting at %d --- %p\n", idx, path_map + idx);
+	print_path(path_map[idx], param->height + 1);
+	dump_bucket(0);
+}
+
+/* pre-compute the paths from root to leaves */
+static void leaf_path(int curr_node, int curr_height, int tree_height, int *path) {
+	path[curr_height] = curr_node;
+	if (curr_height == tree_height) {
+		insert_path(path, curr_node - param->leaf_offset);
+		return;
+	}
+	curr_height++;
+	leaf_path(2 * curr_node + 1, curr_height, tree_height, path);
+	leaf_path(2 * curr_node + 2, curr_height, tree_height, path);
+}
+
+static int init_position_map(int blocks) {
+	int i, tree_height;
+	unsigned long total_buckets;
+	tree_height = param->height;
+	total_buckets = (unsigned long)pow(2., (double)tree_height + 1) - 1;
+	buckets = malloc(sizeof(struct bucket *) * (total_buckets));
+	for (i = 0; i < total_buckets; i++) {
+		struct bucket *bkt = malloc(sizeof(struct bucket));
+		memset(bkt->b_list, -1, sizeof(uint32_t) * BUCKET_SIZE);
+		buckets[i] = bkt;
+	}
+	/* position map
+	 * 0 -> leaf 3
+	 * 1 -> leaf 4
+	 * each block is mapped to a leaf node */
+	pos_map = malloc(sizeof(int) * blocks);
+	printf("%s:%d:tree_height = %d, total_buckets = %ld\n", __func__, __LINE__, tree_height, total_buckets);
+	for (i = 0; i < blocks; i++) {
+		/* must be a leaf node hence the offset after random draw  */
+		pos_map[i] = get_random_leaf(param);
+		printf("%s:%d: [%d] -> [%d]\n", __func__, __LINE__, i, pos_map[i]);
+	}
+	int *path = malloc(sizeof(int) * (tree_height + 1));
+	leaf_path(0, 0, tree_height, path);
+	free(path);
+	return 0;
+}
+
 
 static int _create_oram_tree(char *img, loff_t file_size, int block_size, int bucket_size) {
 	loff_t img_size;
-	int blks, ret, img_file;
+	int blks, ret, img_file, height;
 	img_file = open(img, O_RDWR|O_CREAT);
 	if (ret < 0) {
 		printf("error creating oram image!\n");
 		exit(0);
 	}
-	/* storage size is bucket_size * number_of_blocks */
-	img_size = ((int)(file_size/block_size) + 1) * bucket_size;
+	/* storage size is at least bucket_size * number_of_blocks */
+	param = malloc(sizeof(struct oram_tree_param));
+	blks = (int)(file_size/block_size) + 1;
+	img_size = blks * bucket_size;
+	height  = (int)log2((double)blks) + 1;
+	printf("%s:%d: file size = %ld, blocks = %d\n", __func__, __LINE__, file_size, blks);
 	ret = fallocate(img_file, 0, 0, img_size);
+	param->blocks = blks;
+	param->block_size = block_size;
+	param->height = height;
+	param->leaf_offset = (int)pow(2., height) - 1;
+	stash = malloc(sizeof(struct stash));
+	/* take a number from Fig 3 of the paper */
+	stash->size = 20;
+	stash->b_list = malloc(sizeof(int) * (stash->size));
+	path_map = malloc(sizeof(int *) * param->leaf_offset);
+	/* init position map for client */
+	init_position_map(blks);
+}
+
+static void oram_access(int op, int blk_id, void *buf) {
+	int *path;
+	int x = pos_map[blk_id];
+	/* remap */
+	pos_map[blk_id] = get_random_leaf(param);
+	printf("picking %d leaf...\n", x);
+	path = path_map[x - param->leaf_offset];
+	print_path(path, param->height + 1);
+	for (int i = 0; i < param->height + 1; i++) {
+		int bid = path[i];
+		/* read into stash */
+		printf("read %d bucket into stash\n", bid);
+		dump_bucket(bid);
+	}
+
+	if (op == WRITE) {
+		/* copy data to be written into block on stash */
+	}
+
+	for (int i = param->height; i >= 0; i--) {
+		int *_s = malloc(sizeof(int) * 10);
+		/* generate new path */
+	}
+
+
 
 
 }
@@ -23,7 +166,18 @@ static int _create_oram_tree(char *img, loff_t file_size, int block_size, int bu
 int create_oram_tree(char *orig_file) {
 	loff_t size;
 	int ret = open(orig_file, O_RDONLY);
-	size = lseek(ret, 0, SEEK_END);
-	_create_oram_tree("oram.img", size, 1 << 9, 4);
+	if (ret > 0) {
+		size = lseek(ret, 0, SEEK_END);
+	} else {
+		/* test only */
+		size = 512 * 10;
+	}
+	_create_oram_tree("oram.img", size, 1 << 9, BUCKET_SIZE);
+	return 0;
+}
+
+int main() {
+	create_oram_tree('test.txt');
+	oram_access(0, 0, NULL);
 	return 0;
 }
--- scripts/enigma_test/strawman/oram.h
+++ scripts/enigma_test/strawman/oram.h
@@ -1,1 +1,26 @@
+#include <stdint.h>
+#define BUCKET_SIZE 4
+#define BLOCK_SIZE	(1 << 9)
+#define READ	0
+#define WRITE	1
+
+
+struct bucket {
+	uint32_t b_list[BUCKET_SIZE];
+};
+
+struct oram_tree_param {
+	int height;
+	int block_size;
+	int blocks;
+	int leaf_offset;
+	int *pos_map;
+	int *stash;
+};
+
+struct stash {
+	int *b_list;
+	int size;
+};
+
 int create_oram_tree(char *orig_file);
